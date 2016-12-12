ab655938d363f9024c5c91cf3e0a04908ecfc360
iidzuka <taki.hiyoshi@gmail.com>
2016-12-12 11:47:33 +0900

    lint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,11 +14,11 @@ import pug from 'gulp-pug';
 
 import webpackConfig from './webpack.config.babel';
 
-
-
 const paths = {
   allSrcJs: 'src/**/*.js',
   allLibTests: 'lib/test/**/*.js',
+  serverSrcJs: 'src/server/**/*.js',
+  sharedSrcJs: 'src/shared/**/*.js',
   clientEntryPoint: 'src/client/app.js',
   gulpFile: 'gulpfile.babel.js',
   webpackFile: 'webpack.config.babel.js',
@@ -42,18 +42,18 @@ gulp.task('build', ['lint', 'clean', 'pug', 'sass'], () =>
     .pipe(gulp.dest(paths.libDir)),
 );
 
-gulp.task('main', ['test'], () => {
+gulp.task('main', ['test'], () =>
   gulp.src(paths.clientEntryPoint)
     .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(paths.js));
-});
+    .pipe(gulp.dest(paths.js)),
+);
 
 gulp.task('pug', () =>
   gulp.src(paths.pug)
 /*    .pipe(plumber({
       errorhandler: notify.onError('Error: <%= error.message%>')
     }))*/
-    .pipe(pug({pretty: true}))
+    .pipe(pug({ pretty: true }))
     .pipe(gulp.dest(paths.html)),
 );
 
